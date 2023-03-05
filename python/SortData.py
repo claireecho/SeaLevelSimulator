546cdad2fb7afaf2502e6a9d41ea1e60673137ac
Claire <45241465+claireecho@users.noreply.github.com>
2023-03-05 01:42:10 -0500

    Update SortData.py

--- python/SortData.py
+++ python/SortData.py
@@ -6,14 +6,16 @@ sortData = []
 file=''
 i=0
 #d.remove(",")
+
+# Adding text to a 2D array
 while i< len(d):
     temp = []
     for j in range(3):
         temp.append(float(d[i]))
         i+=1
     sortData.append(temp)
-#print(sortData)
 
+# Sorting Method
 for i in range(len(sortData)):
     for j in range(i, len(sortData)):
         if sortData[i][2] > sortData[j][2]:
