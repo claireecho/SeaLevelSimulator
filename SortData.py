efc33c6359d784c4cdbf67117d49680677612813
Claire <45241465+claireecho@users.noreply.github.com>
2023-03-05 01:31:27 -0500

    Sorted Array

---
+++ SortData.py
@@ -0,0 +1,28 @@
+from array import *
+h = open("xyz.txt", "r").read()
+d= h.split()
+sd = open("Sxyz.txt", "w")
+sortData = []
+i=0
+#d.remove(",")
+while i< len(d):
+    temp = []
+    for j in range(3):
+        temp.append(float(d[i]))
+        i+=1
+    sortData.append(temp)
+#print(sortData)
+
+for i in range(len(sortData)):
+    for j in range(i, len(sortData)):
+        if sortData[i][2] > sortData[j][2]:
+            temp = sortData[i]
+            sortData[i] = sortData[j]
+            sortData[j] = temp
+
+print(sortData)
+
+
+
+sd.write(str(sortData)+ "\n")
+    
