90ce02bb6536ed44a11c25ca64675e3ef107d1ec
Claire <45241465+claireecho@users.noreply.github.com>
2023-03-05 01:36:37 -0500

    changed SortData.py and Sxyz.txt location

--- python/SortData.py
+++ python/SortData.py
@@ -3,17 +3,23 @@ h = open("xyz.txt", "r").read()
 d= h.split()
 sd = open("Sxyz.txt", "w")
 sortData = []
+file=''
 i=0
-while i< len(d)/1000:
+#d.remove(",")
+while i< len(d):
     temp = []
     for j in range(3):
         temp.append(float(d[i]))
         i+=1
     sortData.append(temp)
-def sortby(sortData):
-    sortData.sort(key = lambda x: x[2])
-    return sortData
-print(sortData)
+#print(sortData)
+
+for i in range(len(sortData)):
+    for j in range(i, len(sortData)):
+        if sortData[i][2] > sortData[j][2]:
+            temp = sortData[i]
+            sortData[i] = sortData[j]
+            sortData[j] = temp
 
 
 sd.write(str(sortData)+ "\n")
