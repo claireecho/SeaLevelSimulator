5cbe0fa2b7ead1121c952348f518bef5ff996e1c
Sydney-Buhler <sydneymbuhler@gmail.com>
2023-03-05 02:07:06 -0500

    Create SortData2.py

---
+++ python/SortData2.py
@@ -0,0 +1,36 @@
+from array import *
+h = open("xyz.txt", "r").read()
+d= h.split()
+sd = open("Sxyz.txt", "w")
+ad = open("Axyz.txt", "w")
+sortData = []
+adData = []
+i=0
+#d.remove(",")
+while i< len(d):
+    temp = []
+    for j in range(3):
+        temp.append(float(d[i]))
+        i+=1
+    sortData.append(temp)
+#print(sortData)
+
+for i in range(len(sortData)):
+    for j in range(i, len(sortData)):
+        if sortData[i][2] > sortData[j][2]:
+            temp = sortData[i]
+            sortData[i] = sortData[j]
+            sortData[j] = temp
+
+sd.write(str(sortData)+ "\n")
+sd2 = open("Sxyz.txt", "r").read().split()
+
+while i < len(sortData):
+    n=0
+    adData.append(((float(sortData[i][0]))+89)*150-1500)
+    i+=1
+
+        
+ad.write(adData)
+
+    
